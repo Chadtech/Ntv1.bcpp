7a757f8f771dab813e4a36a95ae4d6ac523a92d6
Chadtech <chadstearns1@gmail.com>
2015-01-01 21:53:22 -0700

    Wav reader now takes file name argument. Also, much cleaner code

--- wavRead.cpp
+++ wavRead.cpp
@@ -2,19 +2,21 @@
 #include <fstream>
 using namespace std;
 
-int main () {
-  ifstream awav;
-  awav.open("testWav.wav", std::ifstream::in);
+int main (int argumentCount, char * arguments[]) {
+  const char * fileName = arguments[1];
 
-  awav.seekg(0, awav.end);
-  int length = awav.tellg();
-  awav.seekg(0, awav.beg);
+  ifstream wav;
+  wav.open(fileName, std::ifstream::in);
+
+  wav.seekg(0, wav.end);
+  int length = wav.tellg();
+  wav.seekg(0, wav.beg);
 
   int data [length];
 
   int datumIndex = 0;
   while (datumIndex < length){
-    data[datumIndex] = awav.get();
+    data[datumIndex] = wav.get();
     datumIndex++;
   }
 
@@ -40,73 +42,7 @@ int main () {
     datumIndex++;
   }
 
-  datumIndex = 0;
-  while (datumIndex < audioDataLength){
-    std::cout << datumIndex << " " << audioData[datumIndex] << "\n";
-    datumIndex++;
-  }
-
-  //int audioData [length - 36]
-
-  /*datumIndex = 0;
-  while (datumIndex < length){
-    //std::cout << datumIndex << "  " << data[datumIndex] << "\n";
-    datumIndex++;
-  }*/
-
-  /*
-  std::cout << "\n";
-  std::cout << "THIS IS DATA " << std::hex << data;
-  std::cout << "\n";
-  */
-  awav.close();
-
-  /*
-  ifstream awav ("testWav.wav");
-
-  if (awav){
-    std::cout << "THIS IS END : " << awav.end << "\n";
-    awav.seekg(0, awav.end);
-    int length = awav.tellg();
-    std::cout << "THIS IS BEGINNING : " << awav.beg << "\n";
-    awav.seekg(0, awav.beg);
-
-    char * buffer = new char [length];
-
-    std::cout << "Reading " << length << " LONG WAVE FILE \n";
-    awav.read(buffer, length);
-
-    if (awav){
-      std::cout << "CHARS READ EPQF \n";
-      std::cout << "THESERDATUM : \n";
-      std::cout << buffer;
-      std::cout << "\n";
-    }
-    else{
-      std::cout << " CHARS READ NOPE \n";
-    }
-    awav.close();
-    delete[] buffer;
-
-  }
-  */
-
-  /*
-  ifstream awav;
-  awav.open("testWav.wav");
-
-  char data[100];
-  awav >> data;
-  cout << data << "\n";
-  cout << "POSITION?" << awav.seekg(4);
-  awav >> data;
-  cout << data << "\n";
-  awav.seekg(4);
-  awav >> data;
-  cout << data << "\n";
-
-  awav.close();
-  */
+  wav.close();
 
   return 0;    
 }
